2bfb37efd26777318b173736476efc320ead749c
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-13 11:02:21 +0800

    add transformer normalization

--- data/data_loader.py
+++ data/data_loader.py
@@ -498,9 +498,9 @@ def get_custom_cifar_loader(data_file, batch_size, target_class=6, t_attack='gre
     ])
 
     transform_train = transforms.Compose([
+        transforms.ToTensor(),
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
