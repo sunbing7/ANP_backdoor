af6bd1713590e897b60cd62fb312a36024c4a928
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-18 12:23:04 +0800

    test

--- cifar_mitigation.py
+++ cifar_mitigation.py
@@ -240,10 +240,11 @@ def analyze_hidden(model, model_name, class_loader, cur_class, num_sample, ana_l
                 for i in range(0, len(dense_hidden_[0])):
                     # x2
                     hidden_do = np.ones(shape=dense_hidden_[:, i].shape)
+                    hidden_do = torch.from_numpy(hidden_do).to(device)
                     #hidden_do = dense_hidden_[:, i] * 2
                     dense_output_ = torch.clone(dense_hidden_)
-                    #dense_output_[:, i] = hidden_do
-                    dense_output_[:, i] = dense_output_[:, i] + torch.from_numpy(hidden_do)
+                    dense_output_[:, i] = dense_output_[:, i] + hidden_do
+
                     dense_output_ = torch.reshape(dense_output_, dense_output.shape)
                     dense_output_ = dense_output_.to(device)
                     output_do = model2(dense_output_).cpu().detach().numpy()
