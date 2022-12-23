912944299f5684994463e576ca2a45b5ddd75643
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-23 15:26:28 +0800

    test

--- semantic_mitigation.py
+++ semantic_mitigation.py
@@ -272,7 +272,7 @@ def remove_exp2():
     torch.save(net.state_dict(), os.path.join(args.output_dir, 'model_init.th'))
     cl_loss, cl_acc = test(model=net, criterion=criterion, data_loader=clean_test_loader)
     po_loss, po_acc = test(model=net, criterion=criterion, data_loader=poison_test_loader)
-    logger.info('0 \t None \t None \t None \t       None \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(po_loss, po_acc, cl_loss, cl_acc))
+    logger.info('0 \t None \t None \t None \t         None \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(po_loss, po_acc, cl_loss, cl_acc))
     #'''
     for epoch in range(1, args.epoch):
         start = time.time()
@@ -310,7 +310,7 @@ def remove_exp2():
     #torch.save(net.state_dict(), os.path.join(args.output_dir, 'model_init.th'))
     cl_loss, cl_acc = test(model=net, criterion=criterion, data_loader=clean_test_loader)
     po_loss, po_acc = test(model=net, criterion=criterion, data_loader=poison_test_loader)
-    logger.info('0 \t None \t None \t None \t       None \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(po_loss, po_acc, cl_loss, cl_acc))
+    logger.info('0 \t None \t None \t None \t         None \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(po_loss, po_acc, cl_loss, cl_acc))
     #'''
 
     # save the last checkpoint
