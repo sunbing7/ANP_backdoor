e602d9500e78893715dff46ea946bed3e6c60457
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-23 14:31:56 +0800

    test

--- semantic_mitigation.py
+++ semantic_mitigation.py
@@ -416,7 +416,7 @@ def analyze_source_class2(model, model_name, target_class, potential_target, num
             temp = temp[ind]
 
             # find outlier hidden neurons
-            top_num = int(len(outlier_detection(temp[:, 1], max(temp[:, 1]), th=5, verbose=False)))
+            top_num = int(len(outlier_detection(temp[:, 1], max(temp[:, 1]), th=2, verbose=False)))
             top_neuron = list(temp[:top_num].T[0].astype(int))
             np.savetxt(args.output_dir + "/outstanding_" + "c" + str(source_class) + "_target_" + str(potential_target) + ".txt",
                        top_neuron, fmt="%s")
