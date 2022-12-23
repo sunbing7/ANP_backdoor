ef61ccd88e1c88b7404d498c0324c47ea80a7550
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-23 10:15:00 +0800

    test

--- semantic_mitigation.py
+++ semantic_mitigation.py
@@ -194,7 +194,7 @@ def remove():
         _adjust_learning_rate(optimizer, epoch, args.lr)
         lr = optimizer.param_groups[0]['lr']
         train_loss, train_acc = train(model=net, criterion=criterion, optimizer=optimizer,
-                                      data_loader=train_mix_loader)
+                                      data_loader=train_clean_loader)
 
         cl_test_loss, cl_test_acc = test(model=net, criterion=criterion, data_loader=clean_test_loader)
         po_test_loss, po_test_acc = test(model=net, criterion=criterion, data_loader=poison_test_loader)
