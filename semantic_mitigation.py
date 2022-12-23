9c58ada52e661427bd8c4a37ff0ec2cdb02dc111
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-23 10:31:56 +0800

    test

--- semantic_mitigation.py
+++ semantic_mitigation.py
@@ -185,11 +185,11 @@ def remove():
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.schedule, gamma=0.1)
     #'''
     # Step 3: train backdoored models
-    logger.info('Epoch \t lr \t Time \t PoisonLoss \t PoisonACC \t CleanLoss \t CleanACC')
+    logger.info('Epoch \t lr \t Time \t CleanLoss \t CleanACC \t PoisonLoss \t PoisonACC \t CleanLoss \t CleanACC')
     torch.save(net.state_dict(), os.path.join(args.output_dir, 'model_init.th'))
     cl_loss, cl_acc = test(model=net, criterion=criterion, data_loader=clean_test_loader)
     po_loss, po_acc = test(model=net, criterion=criterion, data_loader=poison_test_loader)
-    logger.info('0 \t None \t None \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(po_loss, po_acc, cl_loss, cl_acc))
+    logger.info('0 \t None \t None \t None \t None \t {:.4f} \t {:.4f} \t {:.4f} \t {:.4f}'.format(po_loss, po_acc, cl_loss, cl_acc))
 
     #'''
     for epoch in range(1, args.epoch):
