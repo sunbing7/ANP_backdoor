839143f767a54e1f02ccc5139cf125300243f816
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-01-12 18:29:46 +0800

    test

--- semantic_mitigation.py
+++ semantic_mitigation.py
@@ -437,7 +437,7 @@ def pre_analysis():
     print('activation shape: {}'.format(act.shape))
     # analyze hidden neuron causal attribution
     hidden_test = np.loadtxt(
-        args.output_dir + "/test_pre0_" + "c" + str(args.potential_source) + "_layer_" + str(args.ana_layer) + ".txt")
+        args.output_dir + "/test_pre0_" + "c" + str(args.potential_source) + "_layer_" + str(args.ana_layer[0]) + ".txt")
     temp = hidden_test[:, [0, (args.potential_target + 1)]]
     print('causal attribution shape: {}'.format(temp.shape))
 
