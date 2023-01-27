8d8a75ba0534557e3f19b6af5ec8fd80b91fe22d
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-01-27 17:31:10 +0800

    trojaning and trojannet

--- semantic_mitigation.py
+++ semantic_mitigation.py
@@ -700,8 +700,8 @@ def analyze_pcc(num_class, ana_layer):
 
             #mat_ori = hidden_test[:, (source_class + 2)]
             for i in range(0, num_class):
-                #if i == source_class:
-                #    continue
+                if i == source_class:
+                    continue
 
                 # test check on average
                 mat_ori = []
