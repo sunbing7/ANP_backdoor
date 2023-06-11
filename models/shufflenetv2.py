83ae3a643f10379088d4548df0d6381d7417e6d2
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-11 21:05:14 +0800

    shufflenetv2

--- models/shufflenetv2.py
+++ models/shufflenetv2.py
@@ -20,6 +20,7 @@ def shufflenetv2(num_classes=10, pretrained=1, norm_layer=nn.BatchNorm2d, **kwar
     )
 
     # replace norm_layer
+    '''
     children = list(net.children())
     nchildren = []
     for c in children:
@@ -33,8 +34,42 @@ def shufflenetv2(num_classes=10, pretrained=1, norm_layer=nn.BatchNorm2d, **kwar
     # NoisyBatchNorm2d(24),
     #net = nn.Sequential(*[*children[:1], NoisyBatchNorm2d(24), *children[2:21], NoisyBatchNorm2d(1024), *children[22:]])
     net = nn.Sequential(*children)
+    '''
+    modules = list(net.children())
+    module1 = modules[:2]
+    module2 = [modules[2]]
+    module3 = [modules[3]]
 
+    net = nn.Sequential(*[*module1, Relu(), *module2, Avgpool2d_n(poolsize=7), Flatten(), *module3])
     return net
+
+
+class Relu(nn.Module):
+    def __init__(self):
+        super(Relu, self).__init__()
+
+    def forward(self, x):
+        x = F.relu(x)
+        return x
+
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return x
+
+
+class Avgpool2d_n(nn.Module):
+    def __init__(self, poolsize=2):
+        super(Avgpool2d_n, self).__init__()
+        self.poolsize = poolsize
+    def forward(self, x):
+        x = F.avg_pool2d(x, self.poolsize)
+        return x
+
 
 def test():
     net = shufflenetv2()
