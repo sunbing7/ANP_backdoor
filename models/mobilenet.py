8e94fdc662bc2938e576969b824a9d94ebb080e7
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-11 20:21:45 +0800

    mobilenet

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -10,12 +10,12 @@ from torchsummary import summary
 
 class Block(nn.Module):
     '''Depthwise conv + Pointwise conv'''
-    def __init__(self, in_planes, out_planes, stride=1):
+    def __init__(self, in_planes, out_planes, stride=1, norm_layer=nn.BatchNorm2d):
         super(Block, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False)
-        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.bn1 = norm_layer(in_planes)
         self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.bn2 = norm_layer(out_planes)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -27,20 +27,20 @@ class MobileNet(nn.Module):
     # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
     cfg = [64, (128,2), 128, (256,2), 256, (512,2), 512, 512, 512, 512, 512, (1024,2), 1024]
 
-    def __init__(self, num_classes=10, **kwargs):
+    def __init__(self, num_classes=10, norm_layer=nn.BatchNorm2d, **kwargs):
         super(MobileNet, self).__init__()
         self.pool_size = 7
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.layers = self._make_layers(in_planes=32)
+        self.bn1 = norm_layer(32)
+        self.layers = self._make_layers(in_planes=32, norm_layer=norm_layer)
         self.linear = nn.Linear(1024, num_classes)
 
-    def _make_layers(self, in_planes):
+    def _make_layers(self, in_planes, norm_layer=nn.BatchNorm2d):
         layers = []
         for x in self.cfg:
             out_planes = x if isinstance(x, int) else x[0]
             stride = 1 if isinstance(x, int) else x[1]
-            layers.append(Block(in_planes, out_planes, stride))
+            layers.append(Block(in_planes, out_planes, stride, norm_layer=norm_layer))
             in_planes = out_planes
         return nn.Sequential(*layers)
 
