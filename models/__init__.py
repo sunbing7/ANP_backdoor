ae3032b97ea4bd4b25c2066cc6cfdd6b4dc5d7a2
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-11 20:01:33 +0800

    fmnist

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,7 @@ from .resnet_cifar import *
 from .vgg_cifar import *
 from .mobilenetv2 import *
 from .anp_batchnorm import *
+from .densenet import *
+from .mobilenet import *
+from .shufflenetv2 import *
 
